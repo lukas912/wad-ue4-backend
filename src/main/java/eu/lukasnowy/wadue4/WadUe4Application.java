324961f16ba7445ba60df9b20fbe9eb658736790
lukas nowy <lukas.n912@gmail.com>
2021-12-25 18:28:37 +0100

    Project created

---
+++ src/main/java/eu/lukasnowy/wadue4/WadUe4Application.java
@@ -0,0 +1,13 @@
+package eu.lukasnowy.wadue4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadUe4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WadUe4Application.class, args);
+    }
+
+}
---
+++ src/test/java/eu/lukasnowy/wadue4/WadUe4ApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.lukasnowy.wadue4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WadUe4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
