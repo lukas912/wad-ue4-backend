915ed06e3232e514649c9048b2044033ed125c1a
lukas nowy <lukas.n912@gmail.com>
2021-12-25 21:14:45 +0100

    TaskService implemented

--- src/main/java/eu/lukasnowy/wadue4/Controller/TaskController.java
+++ src/main/java/eu/lukasnowy/wadue4/Controller/TaskController.java
@@ -1,6 +1,8 @@
 package eu.lukasnowy.wadue4.Controller;
 
 import eu.lukasnowy.wadue4.Model.Task;
+import eu.lukasnowy.wadue4.Service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class TaskController {
+
+    private TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
     @GetMapping(path = "/tasks")
     public Iterable<Task> getTasks() {
-        return null;
+        return taskService.getTasks();
     }
 }
---
+++ src/main/java/eu/lukasnowy/wadue4/Service/TaskService.java
@@ -0,0 +1,11 @@
+package eu.lukasnowy.wadue4.Service;
+
+import eu.lukasnowy.wadue4.Model.Task;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    public Iterable<Task> getTasks() {
+        return null;
+    }
+}
